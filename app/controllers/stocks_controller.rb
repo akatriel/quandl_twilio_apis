44b1e828f3c279a837b1ccf92e149bdbc78c74e1
akatriel <michael.c.gabay@gmail.com>
2016-02-17 10:56:28 -0500

    successfully saves ticker symbol.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,14 @@ class StocksController < ApplicationController
 		@user = current_user
 		ticker = params[:ticker]
 		begin
-			dataset = Stock.new.get_dataset "#{@ticker}"
-			stock = Stock.new(stock_params)
+			dataset = Stock.new.get_dataset "#{ticker}"
+			stock = Stock.new
 			stock.symbol = ticker
 			stock.user_id = @user.id
 			stock.save
 		rescue
 			flash[:alert] = "Could not add stock to portfolio."
-			redirect_to users_path(@user)
+			redirect_to user_path(@user)
 		end
 	end
 end
