f3be95f69145c75a955f5b4d3d5ef384e6b2cdec
akatriel <michael.c.gabay@gmail.com>
2016-02-19 12:32:53 -0500

    Formatted sms body.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,30 @@
 class MessagesController < ApplicationController
 	def create
-		account_sid = 'AC43bb8565723b5728e0a1c4a286927135'
-		auth_token = '3ce9a0560df8fcd67a7d520e869a534f'
+		account_sid = ENV['TWILIO_SID']
+		auth_token = ENV['TWILIO_AUTH_TOKEN']
 		@client = Twilio::REST::Client.new account_sid, auth_token
+
+		user = current_user
+		portfolio = user.stocks
+
+
+		body = []
+		stock_str = ""
+		portfolio.each do |stock|
+			stock_str += "Symbol: #{stock.symbol}" + "\n Date: #{stock.date}" + "\n Open: #{stock.open}" + "\n Close: #{stock.close}" + "\n High: #{stock.high}" + "\n Low: #{stock.low}" + "\n Volume: #{stock.volume}"
+			body.push(stock_str)
+		end
+		body = body.join("\n").html_safe	
+		p ">>>>>>>>>>>>>>>>>>>>>>>"
+		p body
+
+
 		@client.messages.create(
 			from: '+18562882747',#provided by twilio
-			to: '+12679718062',
-			body: 'Hey there!'
+			to: "+#{user.phone}",
+			body: "#{body}"
 		)
+		
 		redirect_to user_path(current_user)
 	end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,7 @@ class Stock < ActiveRecord::Base
 
 	Quandl::ApiConfig.api_key = 'exAUgh8NLoYAjuwd22PH'
 	Quandl::ApiConfig.api_version = '2015-04-09'
-	# @database = Quandl::Database.get('WIKI')
+	# @database = Quandl::Database.get('WIKI')	
 
 	def self.get_dataset ticker
 		ticker = ticker.upcase
