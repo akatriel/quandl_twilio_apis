f070dd30a6fb2900d304ed83df6159d25b38b3b6
akatriel <michael.c.gabay@gmail.com>
2016-02-24 15:39:49 -0500

    Added autofucs to ticker input. Changed the way a ticker is displayed in flash messages.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -15,13 +15,15 @@ class StocksController < ApplicationController
 		stock.high = dataset.high
 		stock.low = dataset.low
 		stock.volume = dataset.volume
+
 		@stocks = @user.stocks
+
 		respond_to do |format|
 			if dataset && stock.save 
 				format.js
 				format.html { 
 					redirect_to user_path(@user), 
-					notice: "#{ticker} has been added to your portfolio!"
+					notice: "#{stock.symbol} has been added to your portfolio!"
 				}
 			else
 				flash[:alert] = "Could not add stock to portfolio."
