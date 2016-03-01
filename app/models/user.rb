19790a6494442124612810dee5380adc46ad2ff6
akatriel <michael.c.gabay@gmail.com>
2016-02-29 19:41:54 -0500

    Fixed validation on password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	has_many :stocks
 
 	validates :username, presence: true, uniqueness: true 
-	validates :password, presence: true, length: { in: 6..20 }
+	validates :password, length: { in: 6..20 }
+	validates :password, presence: true, on: :create
 	validates :phone, phone: true
 end
